dc4d8fb7b07f58df93fc0e286f9f34e73abd699d
Kurstch <Kurstch@gmail.com>
2020-05-22 15:52:11 +0300

    Finished leaderboard command

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,23 +1,93 @@
 module.exports = {
     name: 'leaderboard',
-    description: 'send a DM with all user scores',
+    description: 'Reply with a leaderboard of users + user scores, upvotes, downvotes',
     aliases: ['lead'],
-    args: true,
-    usage: '`<server>` or `<#channel name>`',
-    execute(message, args, client) {
+    execute(message, args, discordClient) {
+        if (message.channel.type === 'dm') {
+            return message.channel.send('You must ask for leaderboard in the server for witch you want to get the leaderboard');
+        };
+
         const Discord = require('discord.js');
+        const mongoClient = discordClient.mongoClient;
+        const mongodb = message.guild.id;
+        var mongoCollection;
+        var mongoFilter = {};
+        var embedTitle;
 
-        const userData = client.userData
+        // Set mongoCollection & hasMentions value
+        if (!message.mentions.channels.size) {
+            mongoCollection = 'Users';
+            embedTitle = 'Server leaderboard';
+        }
+        else {
+            if (message.mentions.channels.size > 1) {
+                return message.channel.send('Please only mention only one channel');
+            }
+            mongoCollection = message.mentions.channels.first().id;
+            embedTitle = `${message.mentions.channels.first().name} channel leaderboard`
+        };
 
-        const embed = new Discord.MessageEmbed()
-        .setTitle('Server leaderboard')
-        .addFields(
-            { name: 'user', value: 'Som', inline: true },
-            { name: 'score', value: 'S', inline: true },
-            { name: 'upvotes', value: 'af', inline: true},
-            { name: 'downvotes', value: 'af', inline: true},
-        );
+        discordClient.mongodb_read.read(
+            mongoClient,
+            mongodb,
+            mongoCollection,
+            mongoFilter
+        )
+        .then(data => {
+            var leaderboardData = message.guild.members.cache.map(user => {
+                if (!data.some(e => e._id == user.id)) {
+                    return {
+                        userMention: user,
+                        score: 0,
+                        upvotes: 0,
+                        downvotes: 0
+                    };
+                };
+                let userData = data.find(e => e._id == user.id);
+                return {
+                    userMention: user,
+                    score: userData.score,
+                    upvotes: userData.upvotes,
+                    downvotes: userData.downvotes
+                };
+            });
 
-        message.author.send(embed);
+            if (args.includes('upvotes')) {
+                leaderboardData.sort(function(a, b) {
+                    return a.upvotes - b.upvotes;
+                });
+            }
+            else if (args.includes('downvotes')) {
+                leaderboardData.sort(function(a, b) {
+                    return a.downvotes - b.downvotes;
+                });
+            }
+            else {
+                leaderboardData.sort(function(a, b) {
+                    return a.score - b.score;
+                });
+            };
+            leaderboardData.reverse();
+
+            var usersFieldValue = leaderboardData.map(user => {
+                return user.userMention;
+            });
+            var scoreFieldValue = leaderboardData.map(user => {
+                return user.score;
+            });
+            var upvotesDownvotesFieldValue = leaderboardData.map(user => {
+                return `${user.upvotes}/${user.downvotes}`
+            });
+
+            const embed = new Discord.MessageEmbed()
+            .setTitle(embedTitle)
+            .addFields(
+                {name: 'Users', value: usersFieldValue, inline: true},
+                {name: 'Score', value: scoreFieldValue, inline: true},
+                {name: 'Upvotes/Downvotes', value: upvotesDownvotesFieldValue, inline: true}
+            );
+            return message.channel.send(embed);
+        })
+        .catch(console.error);
     },
 };
