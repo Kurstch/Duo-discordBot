1af35a9216f0afcfc815f7a62ada5bff1f6276f4
Kurstch <Kurstch@gmail.com>
2020-06-08 08:50:14 +0300

    update sidebar style

--- ws/public/layout.js
+++ ws/public/layout.js
@@ -1,7 +1,12 @@
-function loadGuildSelector() {
-    //get all guilds the user is a member in
-    //if user is the guilds owner - add the guild to navigation bar guilds selector 
+function initializeSidebar() {
+    initializeGuildSelect();
+    initializeUser();
+}
 
+async function initializeGuildSelect() {
+    //get all guilds the user is a member in
+    //if user is the guilds owner - add the guild to navigation bar guilds selector
+    
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "https://discordapp.com/api/users/@me/guilds");
     xhr.setRequestHeader("Content-Type", "application/json");
@@ -10,17 +15,50 @@ function loadGuildSelector() {
         var response = JSON.parse(xhr.response);
         if(!response[0]) return;
         response.forEach(guild=>{
-            if(guild.owner) {
-                var selecter = document.createElement("option");
-                selecter.innerHTML = guild.name;
-                selecter.value = guild.id;
-                document.getElementById("guilds").appendChild(selecter);
+            if (guild.owner) {
+                var select = document.createElement("option");
+                select.innerHTML = guild.name;
+                select.value = guild.id;
+                document.getElementById("guilds").appendChild(select);
             }
         });
         if (!window.localStorage.gid) {
             window.localStorage.gid = document.getElementById('guilds').options[0].value;  
         }
-        document.getElementById('guilds').value = window.localStorage.gid;          
+        document.getElementById('guilds').value = window.localStorage.gid;
+    }
+    xhr.send();
+}
+
+function initializeUser() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://discordapp.com/api/users/@me");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Authorization", "Bearer " + window.localStorage.token);
+    xhr.onload = function() {
+        var response = JSON.parse(xhr.response);
+        const userDiv = document.getElementById("user");
+        if (!response.id) {
+            const loginLink = document.createElement("a");
+            loginLink.href = "https://discord.com/api/oauth2/authorize?client_id=710067353227886612&redirect_uri=http%3A%2F%2F192.168.43.243%3A8000&response_type=token&scope=identify%20guilds";
+            loginLink.innerHTML = "login";
+            loginLink.id = "login-link"
+            userDiv.appendChild(loginLink);
+        }
+        else {
+            const userAvatar = document.createElement("img");
+            if (response.avatar != null) {
+                userAvatar.src = `https://cdn.discordapp.com/avatars/${response.id}/${response.avatar}.png`;
+            }
+            else {
+                userAvatar.src = "images/discordDefaultAvatar.png"
+            }
+            userDiv.appendChild(userAvatar); 
+
+            const username = document.createElement("small");
+            username.innerHTML = response.username;
+            userDiv.appendChild(username);
+        }
     }
     xhr.send();
 }
