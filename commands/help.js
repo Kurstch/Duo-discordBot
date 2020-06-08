f095902b5a547c306f7819832a5737a81c7d7498
Kurstch <Kurstch@gmail.com>
2020-06-08 10:22:21 +0300

    update help command

--- commands/help.js
+++ commands/help.js
@@ -3,8 +3,20 @@ module.exports = {
     execute(message, args, app) {
         const embed = new app.discord.MessageEmbed()
         .setTitle('Dashboard')
-        .setURL(app.config.ws.oauth2Redirect);
-
+        .setURL("https://discord.com/api/oauth2/authorize?client_id=710067353227886612&redirect_uri=http%3A%2F%2F192.168.43.243%3A8000&response_type=token&scope=identify%20guilds")
+        .setDescription([
+            'commands:',
+            '',
+            '`_score (optional [@user mention] [#channel mention])`',
+            'Get your score',
+            'Get mentioned user(s) score',
+            'Get score from a specific channel',
+            '',
+            '`_lead (optional [#channel mention] [downvotes])`',
+            'Get server leaderboard',
+            'Get channel leaderboard',
+            'Sort leaderboard by downvotes'
+        ]);
         message.channel.send(embed);
     },
 }
