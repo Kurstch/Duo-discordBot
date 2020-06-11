d3b51260b3cefc53a5d49a5486776c3b72de8804
Kurstch <Kurstch@gmail.com>
2020-06-11 13:36:10 +0300

    update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,6 +5,8 @@ module.exports = {
         .setTitle('Dashboard')
         .setURL('https://discord.com/api/oauth2/authorize?client_id=710067353227886612&redirect_uri=http%3A%2F%2F192.168.43.243%3A8000%2Findex&response_type=token&scope=identify%20guilds')
         .setDescription([
+            'Vote by reacting with 👍 and 👎',
+            '',
             '`_score (optional [@user mention] [#channel mention])`',
             'Get your score',
             'Get mentioned user(s) score',
